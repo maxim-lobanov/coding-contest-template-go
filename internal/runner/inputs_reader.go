291218746ad7dd66ca642258d24d92654791b4a0
Maxim Lobanov <maxim-lobanov@github.com>
2025-12-01 12:21:49 +0100

    improve template

--- internal/runner/inputs_reader.go
+++ internal/runner/inputs_reader.go
@@ -8,7 +8,7 @@ import (
 )
 
 func readMainInputFile(solutionFolder string) ([]string, error) {
-	inputFilePath := filepath.Join(solutionFolder, "input.txt")
+	inputFilePath := filepath.Join(solutionFolder, "main.in")
 
 	if _, err := os.Stat(inputFilePath); err != nil {
 		if os.IsNotExist(err) {
@@ -29,56 +29,40 @@ func readMainInputFile(solutionFolder string) ([]string, error) {
 }
 
 func findAllTestFiles(solutionFolder string) ([]string, error) {
-	testPattern := filepath.Join(solutionFolder, "sample_*.txt")
-	testFiles, err := filepath.Glob(testPattern)
+	testInputPattern := filepath.Join(solutionFolder, "sample_*.in")
+	testInputFiles, err := filepath.Glob(testInputPattern)
 	if err != nil {
 		return nil, fmt.Errorf("error listing files by glob: %w", err)
 	}
 
-	return testFiles, nil
-}
-
-func readTestFile(testFilePath string) ([]string, string, error) {
-	if _, err := os.Stat(testFilePath); err != nil {
-		if os.IsNotExist(err) {
-			return nil, "", fmt.Errorf("error reading test file: file does not exist: %s", testFilePath)
-		} else {
-			return nil, "", fmt.Errorf("error checking file stats: %w", err)
+	foundTests := make([]string, 0, len(testInputFiles))
+	for _, testFile := range testInputFiles {
+		baseTestFile := strings.TrimSuffix(testFile, ".in")
+		expectedOutputFile := baseTestFile + ".out"
+		if _, err := os.Stat(expectedOutputFile); err == nil {
+			foundTests = append(foundTests, baseTestFile)
 		}
 	}
 
-	testFileRaw, err := os.ReadFile(testFilePath)
+	return foundTests, nil
+}
+
+func readTestFile(baseTestFile string) ([]string, string, error) {
+	testInputFile := fmt.Sprintf("%s.in", baseTestFile)
+	testOutputFile := fmt.Sprintf("%s.out", baseTestFile)
+
+	testInputRaw, err := os.ReadFile(testInputFile)
 	if err != nil {
-		return nil, "", fmt.Errorf("error reading file content: %w", err)
+		return nil, "", fmt.Errorf("error reading input file: %w", err)
 	}
 
-	testFileLines := strings.Split(string(testFileRaw), "\n")
-	testFilesLinesCount := len(testFileLines)
-
-	separatorLineIndex := -1
-	for i := testFilesLinesCount - 1; i >= 0; i-- {
-		if isTestSeparatorLine(testFileLines[i]) {
-			separatorLineIndex = i
-			break
-		}
+	testOutputRaw, err := os.ReadFile(testOutputFile)
+	if err != nil {
+		return nil, "", fmt.Errorf("error reading output file: %w", err)
 	}
 
-	if separatorLineIndex == -1 {
-		return nil, "", fmt.Errorf("error parsing test file: missing separator line")
-	}
-	if separatorLineIndex < 1 {
-		return nil, "", fmt.Errorf("error parsing test file: no input lines found")
-	}
-	if separatorLineIndex+1 >= testFilesLinesCount {
-		return nil, "", fmt.Errorf("error parsing test file: missing expected output line")
-	}
+	testInputLines := strings.Split(string(testInputRaw), "\n")
+	testOutputLine := strings.TrimSpace(string(testOutputRaw))
 
-	testInputLines := testFileLines[:separatorLineIndex]
-	outputLine := strings.TrimSpace(testFileLines[separatorLineIndex+1])
-
-	return testInputLines, outputLine, nil
-}
-
-func isTestSeparatorLine(line string) bool {
-	return line == "---OUTPUT---"
+	return testInputLines, testOutputLine, nil
 }
